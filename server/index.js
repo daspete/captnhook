451a0298f3a3996de1066f299eab27429e97692d
daspete <daspetemail@gmail.com>
2018-06-14 15:50:42 +0200

    git hook listener addition

--- server/index.js
+++ server/index.js
@@ -1,11 +1,29 @@
 const express = require('express')
-const jsonServer = require('json-server')
-
+const bodyParser = require('body-parser')
 const app = express()
+const token = process.env.TOKEN;
 
-//app.use('/api', jsonServer.router('database/db.json'))
+app.use(bodyParser.json());
 
+app.get('/hook', (req, res) => {
+    if(req.query.token !== token){
+        return res.sendStatus(401);
+    }
+
+    return res.end(req.query.challenge);
+});
+
+app.post('/hook', (req, res) => {
+    if(req.query.token !== token){
+        return res.sendStatus(401);
+    }
+
+    console.log(req.body);
+
+    res.end('');
+});
+  
 module.exports = {
-    path: '/',
+    path: '/api',
     handler: app
 }
